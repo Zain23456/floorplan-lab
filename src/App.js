da8d45e318c00a5d3ffc6b63d1ffd4f69970e3cd
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-10-10 16:52:32 -0400

    finish Bedroom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Bedroom from './Bedroom';
 import Kitchen from './Kitchen';
 import LivingRoom from './LivingRoom';
 
 function App() {
   return (
     <>
+      <Bedroom bedNum={1} />
       <Kitchen />
       <LivingRoom />
+      <Bedroom bedNum={2} />
+      <Bedroom bedNum={3} />
     </>
   );
 }
